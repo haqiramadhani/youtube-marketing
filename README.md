1ce7fc61320aa8140c5bb2d9be22ae177d5dca2b
Haqi Ramadhani <hakiramadhani@gmail.com>
2021-01-28 15:27:21 +0700

    upload with pyppeteer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # YouTube Marketing Tools
 This is server application for render multiple image into video and upload rendered video to youtube automatically.
+
+## Requirements
+- Python 3
+- FFMPEG installed on your machine
+
+## How To Use
+- clone this repo `git clone https://github.com/haqiramadhani/youtube-marketing.git`
+- copy images or videos on project directory
+- add files txt to shortcodes if needed
+- fill `input.txt` file with your video detail (spintax support)
+- run `python3 youtube.py --help` for get help
+- run the program `python3 youtube.py --type=videos`
--- youtube.py
+++ youtube.py
@@ -2,7 +2,6 @@ import argparse
 import http.client
 import os
 import random
-import sys
 import time
 
 import httplib2
@@ -10,6 +9,9 @@ import spintax
 from googleapiclient import sample_tools
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
+import pyppeteer as puppeteer
+import requests
+import asyncio
 
 RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnected,
                         http.client.IncompleteRead, http.client.ImproperConnectionState,
@@ -21,12 +23,12 @@ RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnecte
 def get_list_files(source_type):
     if source_type == 'videos':
         print('indexing video files ....')
-        return os.listdir('./videos')
+        return [x for x in os.listdir('./videos') if '.' in x]
     if source_type == 'images':
         print('indexing image files ....')
-        return os.listdir('./images')
+        return [x for x in os.listdir('./images') if '.' in x]
     print('indexing video temp files ....')
-    return [x for x in os.listdir('./videos') if x.startswith('t')]
+    return [x for x in os.listdir('./videos') if x.startswith(source_type)]
 
 
 # write file for concat
@@ -43,6 +45,7 @@ def get_args():
     print('reading argument ....')
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', required=True, help='From Video or Images', choices=('videos', 'images'))
+    parser.add_argument('--method', required=False, help='Upload with Puppeteer or API', choices=('puppeteer', 'api'))
     return parser.parse_args()
 
 
@@ -52,22 +55,33 @@ def render(source_type):
     filename = str(time.time()).replace('.', '')
     files = get_list_files(source_type)
     if source_type == 'videos':
+        files = random.sample(files, 15)
+        for file in files:
+            name = os.path.join('.', 'videos', "temp" + str(files.index(file) + 1) + ".ts")
+            print(os.path.join('.', 'videos', file))
+            filepath = os.path.join('.', 'videos', file)
+            os.system("ffmpeg -i \"" + filepath + "\" -c copy -bsf:v h264_mp4toannexb -f mpegts " + name)
+        files = get_list_files('temp')
         write_list_txt(files)
+        print('rendering all video to one file ....')
+        os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy -bsf:a aac_adtstoasc output/' + filename + '.mp4')
     else:
         print('converting image to video file ....')
         maximum = len(files) - 1
         for i in range(random.randint(80, 240)):
             index = random.randint(0, maximum)
-            os.system('ffmpeg -r 1/' + random.randint(5, 15)
+            os.system('ffmpeg -r 1/' + str(random.randint(5, 15))
                       + '-f image2 -s 1920x1080 -i images/' + files[index]
-                      + '-vcodec libx264 -crf 25 -pix_fmt yuv420p video/t'
+                      + '-vcodec libx264 -crf 25 -pix_fmt yuv420p video/temp'
                       + str(time.time()).replace('.', '') + '.mp4')
-        files = get_list_files('t')
+        files = get_list_files('temp')
         write_list_txt(files)
-    print('rendering all video to one file ....')
-    os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy output/' + filename + '.mp4')
+        print('rendering all video to one file ....')
+        os.system('ffmpeg -f concat -safe 0 -i mylist.txt -c copy output/' + filename + '.mp4')
     print('removing temp video file ....')
-    os.system('rm videos/t*')
+    for file in os.listdir('videos'):
+        if file.startswith('temp'):
+            os.remove(os.path.join('videos', file))
     return 'output/' + filename + '.mp4'
 
 
@@ -79,8 +93,8 @@ def auth():
     return services
 
 
-# request upload video
-def upload(services, videos):
+# get video detail
+def spin_video_detail():
     print('indexing shortcodes ....')
     shortcodes = os.listdir('./shortcodes')
     print('reading video description ....')
@@ -89,8 +103,6 @@ def upload(services, videos):
     title = title.replace('\n', '')
     description = f.readline()
     keywords = f.readline()
-    category = 27
-    privacy = 'public'
     print('replacing shortcode ....')
     for shortcode in shortcodes:
         f = open('shortcodes/' + shortcode, 'r')
@@ -112,13 +124,20 @@ def upload(services, videos):
     tags = keywords.split(',')
     description = description.replace('\\n', '\n')
     description = spintax.spin(description)
+    return dict(
+        title=title,
+        desc=description,
+        tags=tags
+    )
+
+
+# request upload video
+def upload(services, videos):
+    detail = spin_video_detail()
+    detail['categoryId'] = 27
+    privacy = 'public'
     body = dict(
-        snippet=dict(
-            title=title,
-            description=description,
-            tags=tags,
-            categoryId=category
-        ),
+        snippet=detail,
         status=dict(
             privacyStatus=privacy
         )
@@ -168,14 +187,47 @@ def resumable_upload(request):
             time.sleep(sleep_seconds)
 
 
+# upload video with puppeteer
+async def upload_with_puppeteer(video_path):
+    detail = spin_video_detail()
+    print('get browser ws endpoint ..')
+    response = requests.get('http://localhost:8080/json/version')
+    web_socket_debugger_url = response.json()['webSocketDebuggerUrl']
+    print('connect to browser ..')
+    browser = await puppeteer.connect({
+        'browserWSEndpoint': web_socket_debugger_url,
+        'defaultViewport': None
+    })
+    pages = await browser.pages()
+    page = pages[len(pages) - 1]
+    print('opening youtube upload page ..')
+    await page.goto("https://youtube.com/upload", {"waitUntil": "networkidle0"})
+    time.sleep(3)
+    print('uploading video file ..')
+    element_upload = await page.querySelector("input[type=\"file\"][name=\"Filedata\"]")
+    await element_upload.uploadFile(video_path)
+    time.sleep(10)
+    print('entering title ..')
+    await page.type(".title-textarea > #container > div > #child-input > #input > #textbox", detail['title'])
+    print('entering description ..')
+    await page.type(".description-textarea > #container > div > #child-input > #input > #textbox", detail['desc'])
+    print('wait for upload complete ..')
+    await page.waitForFunction('!document.querySelector(".progress-label").innerText.includes("%")', {'timeout': 0})
+    print('Upload selesai!!!')
+    print(detail['title'])
+    print(detail['desc'])
+    print(detail['tags'])
+
+
 if __name__ == '__main__':
     print('starting program ....')
-    service = auth()
+    # service = auth()
     args = get_args()
     output = render(args.type)
-    try:
-        upload = upload(service, output)
-        print(upload)
-        resumable_upload(upload)
-    except HttpError as error:
-        print('An HTTP error %d occurred:\n%s' % (error.resp.status, error.content))
+    asyncio.get_event_loop().run_until_complete(upload_with_puppeteer(output))
+    # try:
+    #     upload = upload(service, output)
+    #     print(upload)
+    #     resumable_upload(upload)
+    # except HttpError as error:
+    #     print('An HTTP error %d occurred:\n%s' % (error.resp.status, error.content))
